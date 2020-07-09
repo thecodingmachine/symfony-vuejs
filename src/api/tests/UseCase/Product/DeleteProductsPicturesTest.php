5e81689b2c6baedff3c7a516fca8afa01115f970
Julien Neuhart <j.neuhart@thecodingmachine.com>
2020-07-09 11:36:25 +0200

    Add test for delete products pictures use case

---
+++ src/api/tests/UseCase/Product/DeleteProductsPicturesTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Domain\Model\Storable\ProductPicture;
+use App\Domain\Storage\ProductPictureStorage;
+use App\UseCase\Product\DeleteProductsPictures\DeleteProductsPicturesTaskHandler;
+
+it(
+    'deletes pictures',
+    function (): void {
+        $productPictureStorage = self::$container->get(ProductPictureStorage::class);
+        assert($productPictureStorage instanceof ProductPictureStorage);
+        $deleteProductsPicturesTaskHandler = self::$container->get(DeleteProductsPicturesTaskHandler::class);
+        assert($deleteProductsPicturesTaskHandler instanceof DeleteProductsPicturesTaskHandler);
+
+        $storables = ProductPicture::createAllFromPaths([
+            dirname(__FILE__) . '/foo.png',
+            dirname(__FILE__) . '/foo.jpg',
+        ]);
+        $filenames = $productPictureStorage->writeAll($storables);
+
+        $task = new App\UseCase\Product\DeleteProductsPictures\DeleteProductsPicturesTask($filenames);
+        $deleteProductsPicturesTaskHandler($task);
+
+        foreach ($filenames as $filename) {
+            assertFalse($productPictureStorage->fileExists($filename));
+        }
+    }
+)
+    ->group('product');
