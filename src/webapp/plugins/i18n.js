7fa133540c7538234140a755164e483c07c8eae0
Julien Neuhart <j.neuhart@thecodingmachine.com>
2020-10-14 16:26:16 +0200

    Remove useless arg in i18n plugin

--- src/webapp/plugins/i18n.js
+++ src/webapp/plugins/i18n.js
@@ -1,6 +1,6 @@
 import UpdateLocaleMutation from '@/services/mutations/auth/update_locale.mutation.gql'
 
-export default function ({ app, store, error }) {
+export default function ({ app, store }) {
   // Set the 'Accept-Language' header default value.
   app.$graphql.setHeader('Accept-Language', app.i18n.locale)
 
